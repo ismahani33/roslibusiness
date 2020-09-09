dfca1e03ebc28c3a2f113a2dce5e179fe568836c
ismahani33 <ismahani33@gmail.com>
2020-09-09 11:31:54 +0800

    addition: inventory_additem

--- app/src/main/java/com/example/roslibusiness/inventoryMainpage.kt
+++ app/src/main/java/com/example/roslibusiness/inventoryMainpage.kt
@@ -1,11 +1,21 @@
 package com.example.roslibusiness
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.inventory_mainpage.*
 
 class inventoryMainpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inventory_mainpage)
+
+        btnAddInventory.setOnClickListener {
+            val intent1 = Intent(this, inventory_additem::class.java)
+
+            startActivity(intent1)
+        }
+
     }
 }
